49dd7e55178296cd32028b5552a9ba925f5fbc47
Fletcher243 <mumonrift@gmail.com>
2023-03-10 12:34:59 -0700

    Connect to firebase

--- src/Battlefield.jsx
+++ src/Battlefield.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import { writeUserData } from './database/writeSomething'
 
 class Battlefield extends React.Component {
   render() {
     return (<div className='battlefield'>
-
       This is the Battlefield
     </div>);
   }
