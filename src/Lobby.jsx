5a7354db0551baca5c23bf8c0f19da4c7d15d10e
dani81 <31867232+dani81@users.noreply.github.com>
2023-03-10 13:52:42 -0700

    Getting in sync

--- src/Lobby.jsx
+++ src/Lobby.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 //open page
 class Lobby extends React.Component {
-  render() {
-    const mySpan = <span>This is a span</span>
-    return (<div>
-      This is the Lobby
-      {mySpan}
-    </div>);
-  }
+    render() {
+        const characterPrompt = <div>Choose a Character: {"\t"}
+            <select>
+                <option value="harry">Harry Potter</option>
+                <option value="hermione">Hermione</option>
+                <option value="ron">Ron</option>
+                <option value="neville">Neville</option>
+            </select> </div>
+        return (<div>
+
+            Harry Potter Boardgame Lobby
+            {characterPrompt}
+        
+        </div>);
+    }
 }
 
 export default Lobby
