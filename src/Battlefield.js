5e3adfd5697971584150e99cef9ef2441269b6bb
Fletcher243 <mumonrift@gmail.com>
2023-04-04 12:13:55 -0600

    Change jsx endings to js

--- src/Battlefield.js
+++ src/Battlefield.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { writeUserData } from './database/writeSomething'
 
-class Battlefield extends React.Component {
-  render() {
+const Battlefield = () => {
     return (
     <div> 
       <h1 className='battlefield'>This is the Battlefield</h1>
       <img src="/images/Board.jpg" alt="board"></img>
       
     </div>);
-  }
 }
 
 export default Battlefield
